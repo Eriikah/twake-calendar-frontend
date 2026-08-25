c89415af11cfc4e304be188b54ed28a02134a38a
Camille Moussu <cmoussu@linagora.com>
2026-08-25 17:09:21 +0200

    [#624] freebusy : added unknown status icon and tooltip

--- common/src/components/Attendees/FreeBusyIndicator.tsx
+++ common/src/components/Attendees/FreeBusyIndicator.tsx
@@ -1,5 +1,6 @@
 import { Tooltip } from '@linagora/twake-mui'
 import AccessTimeFilledIcon from '@mui/icons-material/AccessTimeFilled'
+import HelpOutlineOutlinedIcon from '@mui/icons-material/HelpOutlineOutlined'
 import { useI18n } from 'twake-i18n'
 import { FreeBusyStatus } from './useFreeBusy'
 
@@ -12,23 +13,25 @@ export const FreeBusyIndicator: React.FC<FreeBusyIndicatorProps> = ({
   status
 }) => {
   const { t } = useI18n()
+  if (!['busy', 'unknown', 'contact'].includes(status)) return null
 
-  if (status !== 'busy') return null
+  const StatusIcon =
+    status === 'busy' ? AccessTimeFilledIcon : HelpOutlineOutlinedIcon
 
   return (
     <Tooltip
       title={
         <span style={{ display: 'flex', alignItems: 'center', gap: 4 }}>
-          {t('event.freeBusy.busy')}
+          {t(`event.freeBusy.${status}`)}
         </span>
       }
       leaveDelay={2000}
       placement="bottom-start"
       slotProps={{ tooltip: { sx: { opacity: 1, bgcolor: 'grey.900' } } }}
     >
-      <AccessTimeFilledIcon
-        aria-label={t('event.freeBusy.busy')}
-        sx={{ color: 'warning.main' }}
+      <StatusIcon
+        aria-label={t(`event.freeBusy.${status}`)}
+        sx={{ color: status === 'busy' ? 'warning.main' : undefined }}
         style={{
           margin: '0 -6px 0 5px',
           flexShrink: 0
